3ec348a0e8cfda995328f1a75c84e147452a5d30
Chris <elhalabichristian@gmail.com>
2024-04-18 17:32:27 +0300

    importing components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TabViewModule } from 'primeng/tabview';
+import { ClimateChangeComponent } from './components/climate-change/climate-change.component';
+import { CovidChartsComponent } from './components/covid-charts/covid-charts.component';
+import { CustomerSegmentationComponent } from './components/customer-segmentation/customer-segmentation.component';
+import { SentimentAnalysisComponent } from './components/sentiment-analysis/sentiment-analysis.component';
+import { StockMarketComponent } from './components/stock-market/stock-market.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    TabViewModule,
+    CovidChartsComponent,
+    StockMarketComponent,
+    ClimateChangeComponent,
+    SentimentAnalysisComponent,
+    CustomerSegmentationComponent,
+  ],
 })
 export class AppComponent {
   title = 'data-visualization';
